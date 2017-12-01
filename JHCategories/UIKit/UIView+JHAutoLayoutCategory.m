d56ad07e9e8a69b2a8511b2428d510e455d17c80
xjh093 <xjh093@126.com>
2017-12-01 16:08:26 +0800

    add JHLog

--- JHCategories/UIKit/UIView+JHAutoLayoutCategory.m
+++ JHCategories/UIKit/UIView+JHAutoLayoutCategory.m
@@ -16,6 +16,11 @@
 #define JH_HAS_JSCore 0
 #endif
 
+#ifdef DEBUG
+#define JHLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define JHLog(...)
+#endif
 
 @implementation UIView (JHAutoLayoutCategory)
 
